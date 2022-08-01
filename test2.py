951e077c043108a6fe414aeccc740f1e3d856864
kumardeepakg92 <kumardeepakg92@gmail.com>
2022-08-01 22:01:07 +0530

    this is working mongo db insert

--- test2.py
+++ test2.py
@@ -1,12 +1,13 @@
 import pymongo
-client = pymongo.MongoClient("mongodb+srv://root:1234@cluster0.goi2j.mongodb.net/?retryWrites=true&w=majority")
-db = client.test
-print(db)
 
+client = pymongo.MongoClient("mongodb+srv://root1:1234@cluster0.l5frp.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+print(db)
 d = {
-    "name":"sudhanshu",
-    "email" : "sudhanshu@ineuron.ai",
-    "surname" : "kumar"
+    "name":"deepak",
+    "email":"deepak@ineuron.ai",
+    "surname":"kumar"
 }
 db1 = client['mongotest']
 coll = db1['test']
